3931ad1298735049b980c361a92a02329aefdf2d
Dmitry Marakasov <amdmi3@amdmi3.ru>
2014-11-26 19:31:41 +0300

    Add missing includes

--- datamanager.hh
+++ datamanager.hh
@@ -22,6 +22,7 @@
 
 #include <string>
 #include <map>
+#include <functional>
 
 class DataManager {
 protected:
--- interpreter.cc
+++ interpreter.cc
@@ -18,6 +18,7 @@
  */
 
 #include <list>
+#include <algorithm>
 
 #include "logger.hh"
 
